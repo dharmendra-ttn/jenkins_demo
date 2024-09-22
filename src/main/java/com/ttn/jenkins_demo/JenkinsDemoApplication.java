a1d917dd18b0a78eb2a05aa9a9e38fb0be78fe2b
Dharmendra <dharmendra@tothenew.com>
2024-09-22 19:12:57 +0530

    fix docker

--- src/main/java/com/ttn/jenkins_demo/JenkinsDemoApplication.java
+++ src/main/java/com/ttn/jenkins_demo/JenkinsDemoApplication.java
@@ -8,7 +8,7 @@ public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println(sayHello());
-		SpringApplication.run(JenkinsDemoApplication.class, args);
+//		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
 	public static String sayHello() {
